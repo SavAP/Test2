16cd3806455f332046bcddedcc870c77454a806e
SavAP <isavap@gmail.com>
2014-12-17 15:50:28 +0800

    sfg

--- WindowsFormsApplication1/Form1.cs
+++ WindowsFormsApplication1/Form1.cs
@@ -19,7 +19,7 @@ namespace WindowsFormsApplication1
 
         private void button1_Click(object sender, EventArgs e)
         {
-            int i = 21;
+            int i =2;
             textBox1.Text = Convert.ToString(i);
         }
     }
